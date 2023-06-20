d9b2516764853df86d830e84cdbd0b6aa1a905db
bidyutsahoo <bidyutsahoobbsr@gmail.com>
2023-06-20 19:16:55 +0530

    vpc-4-commit

--- my-subprocess.py
+++ my-subprocess.py
@@ -3,7 +3,18 @@ import time
 
 time.sleep (60)
 
+print("Terraform Process Starts.....\n")
+
+print("Terraform fmt Starts.....\n")
 subprocess.run(["terraform", "fmt"], check=True)
+
+print("Terraform init Starts.....\n")
 subprocess.run(["terraform", "init"], check=True)
+
+print("Terraform plan Starts.....\n")
 subprocess.run(["terraform", "plan"], check=True)
+
+print("Terraform apply Starts.....\n")
 subprocess.run(["terraform", "apply","-auto-approve"], check=True)
+
+print("Terraform Process Ends...Test the Infrastructure...\n")
