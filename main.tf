7f3ada72ffd4ab4f5829dfd25f8145607697d393
bidyutsahoo <bidyutsahoobbsr@gmail.com>
2023-06-20 10:59:06 +0530

    vpc-1-commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+  credentials = "D:\\Papa\\MyPrograms\\my-key.json"
+  project     = "learned-vault-387908"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
+#Create NW
+resource "google_compute_network" "my-network2" {
+  name                    = "my-network-2"
+  auto_create_subnetworks = false
+}
+#Create SubNW
+resource "google_compute_subnetwork" "my-subnetwork2" {
+  name          = "my-subnetwork-2"
+  network       = google_compute_network.my-network2.self_link
+  region        = "us-central1"
+  ip_cidr_range = "10.0.0.0/24"
+}
+#Create FW
+resource "google_compute_firewall" "my-firewall2" {
+  name    = "my-firewall-2"
+  network = google_compute_network.my-network2.self_link
+
+  allow {
+    protocol = "TCP"
+    ports    = ["80", "443"]
+
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
